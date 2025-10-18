368f8553626e5e59539d535a597ca3cb65b4d463
Viswanath-Gadde <gaddeviswanath628@gmail.com>
2025-10-18 10:13:55 +0530

    Prepare for Streamlit deployment

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pickle
+import re
+import nltk
+nltk.download("punkt")
+nltk.download("stopwords")
+#loading models
+clf=pickle.load(open("clf.pkl", "rb"))
+tfidf=pickle.load(open("tfidf.pkl", "rb"))
+#web app
+def main():
+    st.title("Resume Screening Application")
+    st.file_uploader("Upload Your Resume",type=["pdf","txt"])
+    
+if __name__=="--main__":
+    main()
